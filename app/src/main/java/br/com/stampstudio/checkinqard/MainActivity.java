986411416eab44ee29970774928e34e983d2a5e6
Pedro Teles <pedroateles@gmail.com>
2018-07-18 20:51:23 -0300

    Change QR library to zxing

--- app/src/main/java/br/com/stampstudio/checkinqard/MainActivity.java
+++ app/src/main/java/br/com/stampstudio/checkinqard/MainActivity.java
@@ -2,12 +2,24 @@ package br.com.stampstudio.checkinqard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnCheckin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCheckin = findViewById(R.id.btnCheckin);
+
+        btnCheckin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
