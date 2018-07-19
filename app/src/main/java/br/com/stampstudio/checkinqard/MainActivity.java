8d064a406da94ec3aaadd45d7f3bf5a46768cec2
Pedro Teles <pedroateles@gmail.com>
2018-07-18 21:29:46 -0300

    Create a method to scan QR code

--- app/src/main/java/br/com/stampstudio/checkinqard/MainActivity.java
+++ app/src/main/java/br/com/stampstudio/checkinqard/MainActivity.java
@@ -1,9 +1,15 @@
 package br.com.stampstudio.checkinqard;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnCheckin;
@@ -18,8 +24,25 @@ public class MainActivity extends AppCompatActivity {
         btnCheckin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Bater Cartão");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result.getContents() != null) {
+            Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
