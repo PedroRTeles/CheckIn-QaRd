e61107f7f37e310980c66b189a93fc04908ffa31
Pedro Teles <pedroateles@gmail.com>
2018-07-22 14:06:32 -0300

    Add Volley library and connection to WS

--- app/src/main/java/br/com/stampstudio/checkinqard/MainActivity.java
+++ app/src/main/java/br/com/stampstudio/checkinqard/MainActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -40,19 +47,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         String KEY = Helper.getConfigValue(this, "qr_key");
+        String URL = Helper.getConfigValue(this, "checkin_url");
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
         if(result.getContents() != null) {
             if(result.getContents().equals(KEY)) {
-                Date date = new Date();
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(date);
+                RequestQueue queue = Volley.newRequestQueue(this);
 
-                int hour = calendar.get(Calendar.HOUR_OF_DAY);
-                int minute = calendar.get(Calendar.MINUTE);
-                int second = calendar.get(Calendar.SECOND);
+                StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                Log.d("Response", response);
+                                Toast.makeText(MainActivity.this,  "Registro realizado com sucesso", Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(this,  "Registro realizado com sucesso", Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                Log.d("Error.Response", error.toString());
+                            }
+                        }
+                ) {
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<>();
+                        params.put("idEmployee", "1");
+                        params.put("checkinType", "1");
+
+                        return params;
+                    }
+                };
+                queue.add(postRequest);
+
             } else {
                 Toast.makeText(this, "QR Code inválido", Toast.LENGTH_SHORT).show();
             }
